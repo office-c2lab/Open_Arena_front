26df923fd807f7e854e8a6d89c315b8f0829a619
Youdongseok <rex010307@gmail.com>
2025-10-13 11:01:57 +0900

    EsLint, Prettier, Tailwind 설정

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold text-blue-200">ARENA</h1>
+      <p className="text-xl font-bold text-white-200"> 글자 색 바뀌면 성공</p>
+    </div>
+  );
+}
